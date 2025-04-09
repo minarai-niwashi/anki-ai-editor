f44e283f2c9638445fe384044dc3fa0e857c0816
minarai-niwashi <58800220+minarai-niwashi@users.noreply.github.com>
2025-04-09 22:35:08 +0900

    feat: implement AnkiClient module (#12)

---
+++ src/anki/client.py
@@ -0,0 +1,67 @@
+import json
+import re
+from html.parser import HTMLParser
+
+import requests
+
+from config import ANKI_URL, KEY
+
+
+class HTMLStripper(HTMLParser):
+    def __init__(self):
+        super().__init__()
+        self.reset()
+        self.fed = []
+
+    def handle_data(self, data: str):
+        self.fed.append(data)
+
+    def get_data(self):
+        return "".join(self.fed)
+
+
+def strip_html(html: str) -> str:
+    html = re.sub(
+        pattern=r"<(style|script).*?>.*?</\1>", repl="", string=html, flags=re.DOTALL
+    )
+    s = HTMLStripper()
+    s.feed(data=html)
+    return s.get_data().strip()
+
+
+class AnkiClient:
+    def __init__(self):
+        self.url = ANKI_URL
+        self.key = KEY
+
+    def _invoke(self, action: dict, **param):
+        return requests.post(
+            url=self.url,
+            data=json.dumps(
+                obj={"action": action, "version": 6, "params": param, "key": self.key}
+            ),
+        ).json()
+
+    def get_all_cards(self):
+        query = "deck:*"
+        card_ids = self._invoke(action="findCards", query=query)["result"]
+        notes = self._invoke(action="cardsInfo", cards=card_ids)["result"]
+        cards = []
+        for note in notes:
+            node_id = note["note"]
+            question = strip_html(html=note["question"])
+            answer = strip_html(html=note["answer"])
+            cards.append(
+                {
+                    "node_id": node_id,
+                    "question": question,
+                    "answer": answer,
+                }
+            )
+        return cards
+
+    def update_note_answer(self, note_id: str, new_answer: str):
+        return self._invoke(
+            action="updateNoteFields",
+            note={"id": note_id, "fields": {"Back": new_answer}},
+        )
